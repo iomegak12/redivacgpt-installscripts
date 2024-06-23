7d25ed53a9b3b09600abb2f7bd8b7fe0908cf2d8
iomegak12 <jd.ramkumar@gmail.com>
2024-06-23 11:46:32 +0530

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
 apt-get update
 
 apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
 
-curl https://pyenv.run | bash -
+curl https://pyenv.run | bash
 
 echo -e 'export PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'eval "$(pyenv init --path)"\neval "$(pyenv init -)"' >> ~/.bashrc
